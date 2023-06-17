fe2dad2aa97305cb45b63a52dd16545380000aea
LJRollerII <ljcoolkid@optonline.net>
2023-06-17 12:58:02 -0400

    Adding code which creates music as you type.

---
+++ Scripts/live_music_typing.py
@@ -0,0 +1,17 @@
+from scamp import *
+
+s = Session()
+
+piano = s.new_part("piano")
+
+
+def keyboard_listener(key, code):
+    if len(key) == 1: 
+        if key.isalnum():
+            piano.play_note(ord(key) - 20, 0.5, 0.06, blocking=False)
+        else:
+            piano.play_note(ord(key), 1.0, 0.06, blocking=False)
+
+
+s.register_keyboard_listener(keyboard_listener)
+s.wait_forever()
